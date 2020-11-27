b5dc456f136ad0e8d5b1de65218bf0d8dd1f01ba
JannikBirn <58862960+JannikBirn@users.noreply.github.com>
2020-11-27 15:16:02 +0100

    New TestScene, New BlockGenerator
    Done:
    - Generating Cordinates for the Blocks on a sphere
    - Blocks will be transformted/rotated around the Generator Object
    
    TODO:
    - Fix Block scaling in z direction and radius (needs to be more round, more sphericle)
    - Add depth in y direction
    - Add block cloning
    - Blocks need to be rotated towards the player ?

---
+++ VR_Gruppe_L_Brick_Breaker/Assets/ScriptableObjects/BlockGeneratorSettings.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BlockGeneratorSettings", menuName = "States/BlockGeneratorSettings")]
+public class BlockGeneratorSettings : ScriptableObject
+{
+    //Script for holding the settings for the BlockGenerator
+    //To create a new level you can just create a new BlockGeneratorSettings SO. and change the settings
+
+
+
+    //Settings
+    public Vector3Int blockCount;
+
+}
