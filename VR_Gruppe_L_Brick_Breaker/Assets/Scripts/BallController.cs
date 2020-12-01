64e6ed2c019056d2a3713711a96e919159598f5e
Affenprinz <Tiffany_Bornhof@gmx.de>
2020-12-01 15:00:39 +0100

    Add auto-pilot effect
    For a limited time, the ball moves on its own without returning to the player every time

--- VR_Gruppe_L_Brick_Breaker/Assets/Scripts/BallController.cs
+++ VR_Gruppe_L_Brick_Breaker/Assets/Scripts/BallController.cs
@@ -4,6 +4,7 @@ public class BallController : MonoBehaviour
 {
 
     public Vector3 direction = new Vector3(0f, 0f, 2.5f);
+    public bool isAutoPilot = false;
 
     private GameObject player;
 
@@ -27,34 +28,60 @@ public class BallController : MonoBehaviour
 
             if (collision.collider.CompareTag("Platform"))
             {
-                // When colliding with the player, the reflection axis depends
-                // on where the ball hits the platform. The further away from the center,
-                // the more this axis is twisted
                 Vector3 platformCenterPosition = collision.transform.position;
-                Vector3 centerNormal = contactPoint.normal;
-                Vector3 contactPosition = contactPoint.point;
-
-                // From the contact position, move along the platform's normal vector
-                // and use the end point of that vector to create the reflection axis from the center point
-                Vector3 reflectionAxis = (contactPosition + centerNormal) - platformCenterPosition;
-                Vector3 normal = reflectionAxis.normalized;
-
-                // When the ball collides with the platform,
-                // change its direction using accurate physics
-                // (https://math.stackexchange.com/a/13263)
-                // Calculate reflection vector and ball's new direction
-                Vector3 reflection = direction - 2 * (Vector3.Dot(direction, normal)) * normal;
-                this.direction = reflection;
+                HandlePlatformCollision(platformCenterPosition, contactPoint);
             }
             else if (collision.collider.CompareTag("Brick"))
             {
-                // When the ball collides with a brick, always reflect it back towards the player,
-                // no matter what the previous direction was. Keep the previous speed however,
-                // so normalize the reflected vector and scale it up again
-                Vector3 reflection = player.transform.position - contactPoint.point;
-                float speed = direction.magnitude;
-                this.direction = reflection.normalized * speed;
+                // When colliding with a brick, check if there is an auto-pilot effect
+                // attached to that brick. If this is the case, immediately switch
+                // on the auto pilot mode
+                if (collision.collider.GetComponent<AutoPilotEffect>() != null)
+                {
+                    this.isAutoPilot = true;
+                }
+
+                // During auto pilot, reflect the ball back along the normal vector
+                // and don't send it back to the player
+                if (this.isAutoPilot)
+                {
+                    // Change its direction using accurate physics
+                    // (https://math.stackexchange.com/a/13263)
+                    // Calculate reflection vector and ball's new direction
+                    Vector3 normal = contactPoint.normal;
+                    this.direction = direction - 2 * (Vector3.Dot(direction, normal)) * normal;
+                }
+                else
+                {
+                    // When the ball collides with a brick, always reflect it back towards the player,
+                    // no matter what the previous direction was. Keep the previous speed however,
+                    // so normalize the reflected vector and scale it up again
+                    Vector3 reflection = player.transform.position - contactPoint.point;
+                    float speed = direction.magnitude;
+                    this.direction = reflection.normalized * speed;
+                }
             }
         }
+    }
+
+    private void HandlePlatformCollision(Vector3 platformCenterPosition, ContactPoint contactPoint)
+    {
+        // When colliding with the player, the reflection axis depends
+        // on where the ball hits the platform. The further away from the center,
+        // the more this axis is twisted
+        Vector3 centerNormal = contactPoint.normal;
+        Vector3 contactPosition = contactPoint.point;
+
+        // From the contact position, move along the platform's normal vector
+        // and use the end point of that vector to create the reflection axis from the center point
+        Vector3 reflectionAxis = (contactPosition + centerNormal) - platformCenterPosition;
+        Vector3 normal = reflectionAxis.normalized;
+
+        // When the ball collides with the platform,
+        // change its direction using accurate physics
+        // (https://math.stackexchange.com/a/13263)
+        // Calculate reflection vector and ball's new direction
+        Vector3 reflection = direction - 2 * (Vector3.Dot(direction, normal)) * normal;
+        this.direction = reflection;
     }
 }
