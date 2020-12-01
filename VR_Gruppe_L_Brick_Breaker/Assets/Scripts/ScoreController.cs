575fa33ad4f33c04743158ad71340c391e438174
GeSoFe <gesche1999@gmail.com>
2020-12-01 18:15:23 +0100

    Text nicht angezeigt

---
+++ VR_Gruppe_L_Brick_Breaker/Assets/Scripts/ScoreController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreController : MonoBehaviour
+{
+    public int score = 0;
+    public TextMesh mesh;
+
+    public string text = "Score: ";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh.text = text + score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
