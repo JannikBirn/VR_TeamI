842cf454e75f1f93e7baa8fc44e3296b7430d2fd
Affenprinz <Tiffany_Bornhof@gmx.de>
2020-12-02 15:41:29 +0100

    Render block generator's center sphere with transparent color

--- VR_Gruppe_L_Brick_Breaker/Assets/Scripts/BlockGenerator/BlockGeneratorScript.cs
+++ VR_Gruppe_L_Brick_Breaker/Assets/Scripts/BlockGenerator/BlockGeneratorScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class BlockGeneratorScript : MonoBehaviour
 {
+    private static readonly Color SPHERE_COLOR = new Color(1f, 1f, 1f, 0.3f);
 
     [Header("Setting for each Spherelayer")]
     public SphereSettings[] sphereSettings;
@@ -255,7 +256,7 @@ public class BlockGeneratorScript : MonoBehaviour
         }
 
         //Drawing a green sphere in the center
-        Gizmos.color = Color.white;
+        Gizmos.color = SPHERE_COLOR;
         Gizmos.DrawSphere(this.transform.position, blockSize.x * 2);
     }
 
