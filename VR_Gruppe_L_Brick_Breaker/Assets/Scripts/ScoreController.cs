9e0c8b9c557cd6c54fd2ba1ab0afdfbb68112131
GeSoFe <gesche1999@gmail.com>
2020-12-04 12:52:24 +0100

    ScoreController is Ready!

---
+++ VR_Gruppe_L_Brick_Breaker/Assets/Scripts/ScoreController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreController : MonoBehaviour
+{
+    int score = 0;
+    public TextMesh mesh;
+
+    public string text = "Score: ";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mesh.text = text + score;
+    }
+
+    public void SetScore(int newScore)
+    {
+        score = newScore;
+    }
+
+    public void AddToScore(int newScore)
+    {
+        score += newScore;
+    }
+
+    public int GetScore()
+    {
+        return score;
+    }
+}
