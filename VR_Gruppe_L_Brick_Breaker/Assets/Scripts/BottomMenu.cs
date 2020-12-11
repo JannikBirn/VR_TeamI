2620a66e98fd0119820cf611340270eeb3277c4e
JannikBirn <58862960+JannikBirn@users.noreply.github.com>
2020-12-11 16:48:55 +0100

    Changed Buttons and Added Score Event

--- VR_Gruppe_L_Brick_Breaker/Assets/Scripts/BottomMenu.cs
+++ VR_Gruppe_L_Brick_Breaker/Assets/Scripts/BottomMenu.cs
@@ -18,9 +18,9 @@ public class BottomMenu : MonoBehaviour
     public GameObject menu;
     private bool menuIsShowing = false;
     public GameManager manager;
-        public UnityEvent myEvent;
-     
-    
+    public UnityEvent myEvent;
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -31,11 +31,15 @@ public class BottomMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(gaze) {
-            if(duration > timer) {
-                timer += Time.deltaTime;
-                gazeImg.fillAmount = timer/duration;
-            } else {
+        if (gaze)
+        {
+            if (duration > timer)
+            {
+                timer += Time.unscaledDeltaTime;
+                gazeImg.fillAmount = timer / duration;
+            }
+            else
+            {
                 On();
                 gaze = false;
                 gazeImg.fillAmount = 0;
@@ -44,19 +48,23 @@ public class BottomMenu : MonoBehaviour
 
     }
 
-    public void On() {
+    public void On()
+    {
         GetComponent<Renderer>().material.color = onColor;
         myEvent.Invoke();
     }
-    public void Off() {
+    public void Off()
+    {
         GetComponent<Renderer>().material.color = offColor;
 
-        if(gaze) {
+        if (gaze)
+        {
             gaze = false;
             gazeImg.fillAmount = 0;
         }
     }
-    public void StartCount() {
+    public void StartCount()
+    {
         timer = 0;
         gaze = true;
         gameObject.GetComponent<Renderer>().material.color = waitColor;
--- VR_Gruppe_L_Brick_Breaker/Assets/Scripts/SimpleCameraController.cs
+++ VR_Gruppe_L_Brick_Breaker/Assets/Scripts/SimpleCameraController.cs
@@ -43,7 +43,7 @@ namespace UnityTemplateProjects
                 yaw = Mathf.Lerp(yaw, target.yaw, rotationLerpPct);
                 pitch = Mathf.Lerp(pitch, target.pitch, rotationLerpPct);
                 roll = Mathf.Lerp(roll, target.roll, rotationLerpPct);
-                
+
                 x = Mathf.Lerp(x, target.x, positionLerpPct);
                 y = Mathf.Lerp(y, target.y, positionLerpPct);
                 z = Mathf.Lerp(z, target.z, positionLerpPct);
@@ -55,7 +55,7 @@ namespace UnityTemplateProjects
                 t.position = new Vector3(x, y, z);
             }
         }
-        
+
         CameraState m_TargetCameraState = new CameraState();
         CameraState m_InterpolatingCameraState = new CameraState();
 
@@ -111,7 +111,7 @@ namespace UnityTemplateProjects
             }
             return direction;
         }
-        
+
         void Update()
         {
             Vector3 translation = Vector3.zero;
@@ -122,9 +122,9 @@ namespace UnityTemplateProjects
             if (Input.GetKey(KeyCode.Escape))
             {
                 Application.Quit();
-				#if UNITY_EDITOR
-				UnityEditor.EditorApplication.isPlaying = false; 
-				#endif
+#if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+#endif
             }
             // Hide and lock cursor when right mouse button pressed
             if (Input.GetMouseButtonDown(1))
@@ -143,15 +143,15 @@ namespace UnityTemplateProjects
             if (Input.GetMouseButton(1))
             {
                 var mouseMovement = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y") * (invertY ? 1 : -1));
-                
+
                 var mouseSensitivityFactor = mouseSensitivityCurve.Evaluate(mouseMovement.magnitude);
 
                 m_TargetCameraState.yaw += mouseMovement.x * mouseSensitivityFactor;
                 m_TargetCameraState.pitch += mouseMovement.y * mouseSensitivityFactor;
             }
-            
+
             // Translation
-            translation = GetInputTranslationDirection() * Time.deltaTime;
+            translation = GetInputTranslationDirection() * Time.unscaledDeltaTime;
 
             // Speed up movement when shift key held
             if (Input.GetKey(KeyCode.LeftShift))
@@ -171,8 +171,8 @@ namespace UnityTemplateProjects
 
             // Framerate-independent interpolation
             // Calculate the lerp amount, such that we get 99% of the way to our target in the specified time
-            var positionLerpPct = 1f - Mathf.Exp((Mathf.Log(1f - 0.99f) / positionLerpTime) * Time.deltaTime);
-            var rotationLerpPct = 1f - Mathf.Exp((Mathf.Log(1f - 0.99f) / rotationLerpTime) * Time.deltaTime);
+            var positionLerpPct = 1f - Mathf.Exp((Mathf.Log(1f - 0.99f) / positionLerpTime) * Time.unscaledDeltaTime);
+            var rotationLerpPct = 1f - Mathf.Exp((Mathf.Log(1f - 0.99f) / rotationLerpTime) * Time.unscaledDeltaTime);
             m_InterpolatingCameraState.LerpTowards(m_TargetCameraState, positionLerpPct, rotationLerpPct);
 
             m_InterpolatingCameraState.UpdateTransform(transform);
